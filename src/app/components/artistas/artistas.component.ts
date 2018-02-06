047ac2ce82c16bc745493f659bb34ce2fd7dcb29
Ernesto <ernestoaguaysol@gmail.com>
2018-02-05 22:51:11 -0300

    artistas

--- src/app/components/artistas/artistas.component.ts
+++ src/app/components/artistas/artistas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { GLOBAL } from "../../services/global";
+import { UserService } from "../../services/user.service";
+import { Artist } from '../../models/artist';
 
 @Component({
   selector: 'app-artistas',
@@ -7,7 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistasComponent implements OnInit {
 
-  constructor() { }
+  public titulo: string;
+  public artists: Artist[];
+  public identity;
+  public token;
+  public url: string;
+
+  constructor(private _userService: UserService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.titulo = 'Artistas';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.url = GLOBAL.url;
+  }
 
   ngOnInit() {
   }
